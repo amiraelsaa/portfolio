5edf57836670b6791a0cf690e0107e5dcb073932
amiraelsaa <amiraelsaa851@gmail.com>
2023-05-14 14:52:52 +0300

    project

---
+++ js/script.js
@@ -0,0 +1,21 @@
+jQuery(function($){
+    $( '.menu-btn' ).click(function(){
+    $('.drop-down').toggleClass('list-active')
+})
+})
+
+//accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+    });
+}
